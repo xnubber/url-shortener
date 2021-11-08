5d04df4fe9a8a4cd9e3be479cb922edbd02eedce
xnubber <aabbccdd10902@gmail.com>
2021-11-08 22:02:40 +0800

    add express handlebars & mongoose

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
+// express
 const express = require('express')
 const app = express()
 const port = 3000
 
+// express handlebars
+const exphbs = require('express-handlebars')
+app.engine('hbs', exphbs({defaultLayout: 'main', extname:'.hbs'}))
+app.set('view engine', 'hbs')
+
+// mongoose
+require('./config/mongoose')
+const UrlShortener = require('./models/url-shortener')
+
+// tools
+app.use(express.urlencoded({extended: true}))
+
+// routes
 app.get('/', (req, res) => {
-  res.send('hi')
+  console.log(window.location.hostname)
+  res.render('index')
+})
+
+app.post('/', async (req, res) => {
+  const newShortUrl = new UrlShortener(req.body)
+  await newShortUrl.save()
+  res.redirect('/')
 })
 
 app.listen(port, () => {
---
+++ models/url-shortener.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const urlShortenerSchema = new mongoose.Schema({
+  origin_url: {
+    type: String,
+    required: true,
+  },
+  shorten_url: {
+    type: String,
+  },
+  custom: {
+    type: String,
+  },
+  password: {
+    type: String,
+  },
+  QR_code: {
+    type: String
+  },
+})
+
+const UrlShortener = mongoose.model('UrlShortener', urlShortenerSchema)
+module.exports = UrlShortener
